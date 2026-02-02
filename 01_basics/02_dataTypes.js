d49bf2f4a81bdf6daba6892a4e148db4ef0e229a
Aman Verma <857449av@gmail.com>
2026-02-03 00:11:38 +0530

    lec 5 done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version of JS
+
+//alert(3+3)//we arae using nodejs not browser 
+
+console.log(3
+    +
+    3
+)//code readibility important/should be high
+console.log("Aman");
+//original documentation tc39.es
+
+
+let name="Aman"
+let age=20
+let isLoggedIn=false
+
+//different datatype
+//number=>2 to the power 53
+//for big no more than this use bigint
+//string=>""
+//boolean=>true/false
+//numm=>standalone value
+//undefined =>variable whose value is not defined
+
