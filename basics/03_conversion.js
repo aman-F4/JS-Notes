8ba5e0bc009f527717ce5d2dda700fde0e589ffb
Aman Verma <857449av@gmail.com>
2026-02-03 11:49:18 +0530

    lec 7 done

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -26,4 +26,43 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+// **************OPERATIONS****************
+ 
+let value=3;
+let negValue=-value;
+console.log(negValue);
+
+//console.log(2**3) means 2 to the power 3 
+
+let str1="hello";
+let str2=" aman";
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+//agar string phle h to sbhi string me convert ho jayega
+//else agr last me string h to phle number ka jo bhi operation hoga uske baad string aa jayega
+
+
+console.log(+true);//gives 1
+console.log(+"");// gives 0 but not a good way to write this type of code
+
+let num1,num2,num3;
+num1=num2=num3=2+2-1;
+console.log(num3);//give 3 not a good way
+
+let gameCounter=100;
+//++gameCounter isme value phle increament ho jati h
+gameCounter++;
+console.log(gameCounter);
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
 
